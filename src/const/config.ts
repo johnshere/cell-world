acd1dc88cafdb02f4afc1bd39b5eb11822b0efde
johnshere <john__cloud@163.com>
2025-09-09 22:41:29 +0800

    feat: entity，render

--- src/const/config.ts
+++ src/const/config.ts
@@ -3,5 +3,5 @@ export const RootEl = document.getElementById('app')!;
 // 帧率控制
 export const FrameRate = 10;
 
-export const GridSize = 20;
+export const GridSize = 15;
 export const GridColor = '#ccc';
